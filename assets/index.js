232fc81d5c1d055a3f9909969f2e98f3d72acb03
Benjamin Kantor <Meepasaurus@users.noreply.github.com>
2016-08-17 23:51:34 -0400

    working formatted timer
    base functionality working

--- assets/index.js
+++ assets/index.js
@@ -1,51 +1,104 @@
 'use strict';
 
-var Timer = function(){
-	var slice = $('#slice'),
-		seconds = 0; //0-59
+var Timer = function(defaultSeconds,sliceDOM, counterDOM){
+	var slice = sliceDOM,
+		counter = counterDOM,
+		seconds = defaultSeconds,
+		interval = '',
+		isActive = false;
 
 	return {
-		drawSlice: function(seconds){
-			//1/60 seconds = 6/360 degrees
-			var degrees = 6 * seconds + 90;
+		updateCounter: function(){
+			var secPrefix = '',
+          		minPrefix = '',
+          		minutesToRefresh = Math.floor(seconds/60),
+	      		secondsToRefresh = seconds % 60;
+	      
+	      	if (secondsToRefresh < 10){
+	        	secPrefix = '0';
+	      	}
+	      	if (minutesToRefresh < 10){
+	        	minPrefix = '0';
+	      	}
+	      
+	      	counter.text(minPrefix + minutesToRefresh + ':' + secPrefix + secondsToRefresh);
+		},
 
-			if (seconds <= 30){
-				//.slice-right is used for 0-50% (90-270deg)
+		updateClock: function(){
+			//1/60 seconds = 6/360 degrees
+			var partialSeconds = seconds % 60; //seconds remaining in the :00 part of the timer
+			var degrees = 6 * partialSeconds + 90;
+
+			//linear gradient method adapted from SO - Sampson
+			if (partialSeconds <= 30){
+				//0-50% (90-270deg)
 				slice.css({
 					'background-image' :
         				'linear-gradient(' + degrees +
-        				'deg, transparent 50%, white 50%),linear-gradient(90deg, white 50%, transparent 50%)'
+        				'deg, transparent 50%, green 50%),linear-gradient(90deg, green 50%, transparent 50%)'
 				});
 			} else {
 				degrees -= 180;
-				console.log(degrees);
 				//90-270 for 50-100%
 				slice.css({
 					'background-image' :
         				'linear-gradient(' + degrees +
-        				'deg, transparent 50%, green 50%),linear-gradient(90deg, white 50%, transparent 50%)'
+        				'deg, transparent 50%, white 50%),linear-gradient(90deg, green 50%, transparent 50%)'
 				});
 			}
+			//update counter
+			this.updateCounter();
+			
+			if (seconds === 0){
+				this.startStop();
+				console.log('Time\'s up.');
+			} else {
+				seconds--;
+				console.log(seconds+1 + ' seconds remaining.');
+			}
+		},
+
+		startStop: function(){
+			if (isActive){
+				//stop
+				window.clearInterval(interval);
+				isActive = false;
+			} else {
+				//start
+	          	interval = window.setInterval(this.updateClock.bind(this), 1000);
+	          	isActive = true;
+	        }
+		},
+
+		setSeconds: function(newSeconds){
+			if (newSeconds){
+				seconds += 2;
+			}
+			this.updateClock();
+		},
+
+		init: function(){
+			this.updateClock();
 		}
 	};
 };
 
 $(document).ready(function(){
-	var myTimer = new Timer();
+	var myTimer = new Timer(125, $('#slice'), $('.counter').find('p'));
+	myTimer.init();
 
-	var percent = 29;
+	$('.btn-clock').on('click', function(e){
+		e.preventDefault();
+		myTimer.startStop();
+	});
 
 	$('#plus').on('click', function(e){
 		e.preventDefault();
-		percent++;
-		$('.percent').find('p').text(percent);
-		myTimer.drawSlice(percent);
+		myTimer.setSeconds(true);
 	});
 
 	$('#minus').on('click', function(e){
 		e.preventDefault();
-		percent--;
-		$('.percent').find('p').text(percent);
-		myTimer.drawSlice(percent);
+		myTimer.setSeconds(false);
 	});
 });
